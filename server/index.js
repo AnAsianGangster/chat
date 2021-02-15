9315a43ee2a39f9ab82adb62f5c71bbf6a577fac
AnAsianGangster <yangaobo1996@gmail.com>
2021-02-15 22:14:26 +0800

    add server/index.js

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import http from 'http';
+import express from 'express';
+import * as io from 'socket.io';
+// socket configuration
+import WebSockets from '../utils/WebSockets.js';
+
+const app = express();
+
+const port = process.env.port || '5000';
+app.set('port', port);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+/* catch 404, and handover to error handler */
+app.use('*', (req, res) => {
+    return res.status(400).json({
+        success: false,
+        message: "API endpoint doesn't exist",
+    });
+});
+
+/* Create HTTP server */
+const server = http.createServer(app);
+/* Create socket connection */
+const socketio = new io.Server(server);
+global.io = socketio.listen(server);
+global.io.on('connection', WebSockets.connection);
+/* Listen on port, on all network interfaces */
+server.listen(port);
+/* Event listener for HTTP server "listening" event */
+server.on('listening', () => {
+    console.log(`Listening on port:: http://localhost:${port}`);
+});
