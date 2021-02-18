2dbe93b8968e5ee959f713341a970b8214ca01e2
AnAsianGangster <yangaobo1996@gmail.com>
2021-02-18 23:54:55 +0800

    [fix] dynamodb typo & add db connection validation

--- config/dynamodb.js
+++ config/dynamodb.js
@@ -2,11 +2,11 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const DynomdbConfig = {
+const DynamodbConfig = {
     region: process.env.AWS_REGION,
     endpoint: process.env.AWS_ENDPOINT,
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 };
 
-export default DynomdbConfig;
+export default DynamodbConfig;
--- server/index.js
+++ server/index.js
@@ -2,11 +2,10 @@ import AWS from 'aws-sdk';
 import http from 'http';
 import express from 'express';
 import * as io from 'socket.io';
-// socket configuration
+// import WebSocket class
 import WebSockets from '../utils/WebSockets.js';
-
-// dynomdb config
-import DynomdbConfig from '../config/dynomdb.js'
+// import dynomdb config
+import DynamodbConfig from '../config/dynamodb.js';
 
 const app = express();
 
@@ -16,8 +15,40 @@ app.set('port', port);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// update aws with dynamodb config
+AWS.config.update(DynamodbConfig);
 /* --------------test for dynamodb connection ---------- */
-AWS.config.update(DynomdbConfig);
+// TODO remove this part in the future
+const dynamodb = new AWS.DynamoDB();
+const params = {
+    TableName: 'Movies',
+    KeySchema: [
+        { AttributeName: 'year', KeyType: 'HASH' }, //Partition key
+        { AttributeName: 'title', KeyType: 'RANGE' }, //Sort key
+    ],
+    AttributeDefinitions: [
+        { AttributeName: 'year', AttributeType: 'N' },
+        { AttributeName: 'title', AttributeType: 'S' },
+    ],
+    ProvisionedThroughput: {
+        ReadCapacityUnits: 10,
+        WriteCapacityUnits: 10,
+    },
+};
+
+dynamodb.createTable(params, function (err, data) {
+    if (err) {
+        console.error(
+            'Unable to create table. Error JSON:',
+            JSON.stringify(err, null, 2)
+        );
+    } else {
+        console.log(
+            'Created table. Table description JSON:',
+            JSON.stringify(data, null, 2)
+        );
+    }
+});
 /* ----------------------------------------------------- */
 
 /* catch 400, and handover to error handler */
